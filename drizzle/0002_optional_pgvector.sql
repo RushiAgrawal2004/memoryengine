55749ca3f97e7cc1fbd20a7cc6416bbf46f7af79
Rushi Agrawal <rushiagrawal2004@gmail.com>
2026-06-08 22:42:13 +0530

    Make pgvector migrations deploy-safe

--- drizzle/0002_optional_pgvector.sql
+++ drizzle/0002_optional_pgvector.sql
@@ -3,12 +3,3 @@ create extension if not exists vector;
 alter table memories add column if not exists embedding_vector vector;
 alter table entities add column if not exists embedding_vector vector;
 alter table edges add column if not exists embedding_vector vector;
-
-create index if not exists memories_embedding_vector_hnsw_idx
-  on memories using hnsw (embedding_vector vector_cosine_ops);
-
-create index if not exists entities_embedding_vector_hnsw_idx
-  on entities using hnsw (embedding_vector vector_cosine_ops);
-
-create index if not exists edges_embedding_vector_hnsw_idx
-  on edges using hnsw (embedding_vector vector_cosine_ops);
